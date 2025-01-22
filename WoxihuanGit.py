e22f9b66aa76ccede754f04c6b52c243e1fc5cc1
LeoDengAtRectory <jingwen.deng@rectorywolves.org>
2025-01-22 11:05:55 -0500

    Vigenere Cipher created

--- WoxihuanGit.py
+++ WoxihuanGit.py
@@ -1,5 +1,10 @@
-alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-pt=input("Enter the text you are gonna to encode")
-key=input("Enter the key")
+alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+pt=input("Enter the text you are gonna to encode:")
+key=input("Enter the key:")
 ks=""
 ct=""
+for i in range(len(pt)):
+	ks+=key[i%len(key)]
+for i in range(len(pt)):
+	ct+=alph[(alph.index(pt[i])+alph.index(ks[i]))%26]
+print("Your encoded text is:"+ct)
